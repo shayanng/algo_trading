5d986a776ee1727dd8d43eba54f817c380161d35
Shayan <naghizadehsh@gmail.com>
2020-02-06 17:46:23 -0500

    Just a quick update of what has benn thought today

--- web_scraping.py
+++ web_scraping.py
@@ -1,11 +1,23 @@
 #web scrapping
 #%%
 import pandas as pd
-a = 2
-b = 3
-
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ca.finance.yahoo.com/quote/TSLA/balance-sheet?p=TSLA'
-page = requests.get(url)
+tickers = ['AAPL', 'MSFT']
+financial_dir = {}
+
+for ticker in tickers:
+    temp_dir = {}
+    url = 'https://ca.finance.yahoo.com/quote/'+ticker+'/balance-sheet?p='+ticker
+    page = requests.get(url)    
+    page_content = page.content
+    soup = BeautifulSoup(page_content, 'html.parser')
+    table = soup.find_all("div", {"class" : "M(0) Mb(10px) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+    for t in table:
+        rows = t.find_all("div", {"class" : "rw-expnded"})
+        for row in rows:
+            temp_dir[row.get_text(separator='|').split("|")[0]]=row.get_text(separator='|').split("|")[1]
+
+#%%
+            
