ac2ee21a906444dae5fbaa2ccf2add13d266abca
Shayan Naghizadeh <naghizadehsh@gmail.com>
2020-02-11 00:09:29 -0500

    Added the CAGR function to the KPI's python file plus added visualization and test functionality for better understanding

---
+++ KPIs.py
@@ -0,0 +1,29 @@
+#%% Compounded Annual Growth Rate (CAGR)
+
+# CAGR = [end value/beginning value]^(1/years) - 1 
+
+import pandas_datareader.data as web
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+ticker = "^GSPC"
+
+spx = web.get_data_yahoo(ticker, 
+                        datetime.date.today() - datetime.timedelta(1825),
+                        datetime.date.today())
+
+plt.figure(figsize = (12,8))
+spx["Adj Close"].plot()
+plt.show()
+
+def CAGR(df):
+    df = df.copy()
+    df["Daily Ret"] = df["Adj Close"].pct_change()
+    df["Cum Return"] = (1 + df["Daily Ret"]).cumprod()
+    CAGR = df["Cum Return"][-1]**(1/(len(df)/252)) - 1
+    return CAGR
+
+cagr_tst = CAGR(spx)
+print
