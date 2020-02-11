fcf7471b3f2a607abd4aa8c5efa5a054463bb5dd
Shayan <naghizadehsh@gmail.com>
2020-02-11 10:42:18 -0500

    Just finished some of many KPIs and wrote functions of performance matrix in KPIs.py file

--- KPIs.py
+++ KPIs.py
@@ -26,4 +26,96 @@ def CAGR(df):
     return CAGR
 
 cagr_tst = CAGR(spx)
-print
+
+#%% Annualized volatility 
+import pandas_datareader.data as web
+import numpy as np
+import datetime
+
+ticker = "^GSPC"
+
+spx = web.get_data_yahoo(ticker, 
+                        datetime.date.today() - datetime.timedelta(1825),
+                        datetime.date.today())
+
+def annualized_volatility(df):
+    df = df.copy()
+    df["Daily Ret"] = df["Adj Close"].pct_change()
+    vol = df["Daily Ret"].std() * np.sqrt(256)
+    return vol
+
+av_tst = annualized_volatility(spx)
+
+#%% Sharpe & Sortino Ratio
+import pandas_datareader.data as web
+import numpy as np 
+import datetime
+
+ticker = "^GSPC"
+
+spx = web.get_data_yahoo(ticker, 
+                        datetime.date.today() - datetime.timedelta(1825),
+                        datetime.date.today())
+
+## before the calcultion of sharpe ratio we must have the risk-free rate
+risk_free_rate = 0.022
+
+def sharpe_ratio(df, rfr):
+    df = df.copy()
+    sr = (CAGR(df) - rfr)/annualized_volatility(df)
+    return sr
+
+def sortino_ratio(df, rfr):
+    df["Daily Ret"] = df["Adj Close"].pct_change()
+    neg_vol = df[df["Daily Ret"] < 0]["Daily Ret"].std() * np.sqrt(256)
+    sor = (CAGR(df) - rfr)/neg_vol
+    return sor
+    
+    
+sr_tst = sharpe_ratio(spx, risk_free_rate)  
+sor_tst = sortino_ratio(spx, risk_free_rate)
+
+#%% Max Drawdown & Calmar Ratio
+import pandas_datareader.data as web
+import numpy as np 
+import datetime
+
+ticker = "^GSPC"
+
+spx = web.get_data_yahoo(ticker, 
+                        datetime.date.today() - datetime.timedelta(1825),
+                        datetime.date.today())
+
+
+def max_drawdown(df):
+    df = df.copy()
+    df["Daily Ret"] = df["Adj Close"].pct_change()
+    df["Cum Return"] = (1 + df["Daily Ret"]).cumprod()
+    df["Cum Rolling Max"] = df["Cum Return"].cummax()
+    df["Drawdown"] = df["Cum Rolling Max"] - df["Cum Return"]
+    df["Drawdown_Pct"] = df["Drawdown"] / df["Cum Rolling Max"]
+    max_dd = df["Drawdown"].max()
+    return max_dd
+
+def calmar_ratio(df):
+    df = df.copy()
+    calmar = CAGR(df)/max_drawdown(df)
+    return calmar
+
+cal_tst = calmar_ratio(spx)
+dd_tst = max_drawdown(spx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
