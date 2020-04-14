953fccdc875abda99cb4c0652ac59aaf73bf5402
shayanng <52363545+shayanng@users.noreply.github.com>
2020-04-14 12:03:41 -0400

    Update tech_indicators.py

--- tech_indicators.py
+++ tech_indicators.py
@@ -36,8 +36,8 @@ def Bollinger_Bands(df, n):
     """
     df = df.copy()
     df["MA"] = df['Adj Close'].rolling(n).mean()
-    df["BB_up"] = df["MA"] + 2*df['Adj Close'].rolling(n).std(ddof=0) #ddof=0 is required since we want to take the standard deviation of the population and not sample
-    df["BB_dn"] = df["MA"] - 2*df['Adj Close'].rolling(n).std(ddof=0) #ddof=0 is required since we want to take the standard deviation of the population and not sample
+    df["BB_up"] = df["MA"] + 2*df['Adj Close'].rolling(n).std(ddof=0)
+    df["BB_dn"] = df["MA"] - 2*df['Adj Close'].rolling(n).std(ddof=0)
     df["BB_width"] = df["BB_up"] - df["BB_dn"]
     df.dropna(inplace=True)
     return df
